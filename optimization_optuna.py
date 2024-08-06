9cca234cc9f4fc498e16a971463fd80c195763a9
Stefano Genetti <stefanogenetti2000@gmail.com>
2024-08-06 16:58:11 +0200

    update optuna with out dir, reproducible output, multiple run, timeout termination

--- optimization_optuna.py
+++ optimization_optuna.py
@@ -1,17 +1,24 @@
 from typing import List, Dict, Tuple, Set
 from alpypeopt import AnyLogicModel
 from itertools import permutations
+from datetime import datetime
 import optuna
 import matplotlib.pyplot as plt
 import csv
 import argparse
+import os
 import pandas as pd
 
 def read_arguments():
     parser = argparse.ArgumentParser(description="Adige hybrid flow shop scheduling optimizer.")
     
     parser.add_argument("--input_csv", type=str, help="File path of the CSV where the optimization output has been stored.")
-    parser.add_argument("--n_trials", type=int, default=100, help="Number of optimization trials.")
+
+    parser.add_argument("--n_trials", type=int, help="Number of optimization trials.") # default was 100
+    parser.add_argument("--timeout", type=int, help="Stop study after the given number of second(s).")
+
+    parser.add_argument("--out_dir", type=str, help="Output folder.")
+    parser.add_argument('--no_runs', type=int, default=1, help='Number of runs.')
 
     parser.add_argument("--m", type=int, default=1, help="Resources of type M.")
     parser.add_argument("--e", type=int, default=1, help="Resources of type E.")
@@ -26,10 +33,6 @@ def read_arguments():
 if __name__ == '__main__':
     args = read_arguments()
 
-    # history of evaluations
-    history_x:List[List[int]] = []
-    history_y:List[int] = []
-
     if args["input_csv"]:
         csv_file_path = args["input_csv"]
         df = pd.read_csv(csv_file_path)
@@ -40,8 +43,15 @@ if __name__ == '__main__':
         plt.title('Fitness Trend')
         plt.legend()
         plt.grid(True)
-        plt.show()
+        #plt.show()
+        plt.savefig(f"{args['out_dir']}/optuna.pdf")
+        plt.savefig(f"{args['out_dir']}/optuna.png")
     else:
+        # create directory for saving results
+        current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+        output_folder_path = f"{args['out_dir']}/{current_datetime}"
+        os.makedirs(output_folder_path)
+
         adige_model = AnyLogicModel(
             env_config={
                 'run_exported_model': False
@@ -69,8 +79,6 @@ if __name__ == '__main__':
             totalAvailableR : number of resources of type R available
             datasetFilePath : file path of the csv file storing the dataset
             """
-            print(f"x={x}")
-
             # set available resources
             adige_setup.setTotalAvailableM(totalAvailableM)
             adige_setup.setTotalAvailableE(totalAvailableE)
@@ -100,7 +108,9 @@ if __name__ == '__main__':
                       totalAvailableM: int,
                       totalAvailableE: int,
                       totalAvailableR: int,
-                      datasetFilePath: str):
+                      datasetFilePath: str,
+                      history_x: List[List[int]],
+                      history_y: List[int]):
             priorities = [trial.suggest_int(f'{jobs[i]}', 0, len(jobs)) for i in range(len(jobs))]
             jobs_with_priorities = list(zip(jobs, priorities))
             sorted_jobs_with_priorities = sorted(jobs_with_priorities, key=lambda x: x[1], reverse=True)
@@ -114,29 +124,82 @@ if __name__ == '__main__':
             history_y.append(int(y))
             return y
 
-        # run the optimizer
-        study = optuna.create_study(direction="minimize")
-        study.optimize(
-            lambda trial:   objective(  trial=trial,
-                                        jobs=input_jobs,
-                                        totalAvailableM=args["m"],
-                                        totalAvailableE=args["e"],
-                                        totalAvailableR=args["r"],
-                                        datasetFilePath=args["dataset"]),
-                            n_trials=args["n_trials"])
+        for r in range(args["no_runs"]):
+            # create directory for saving results of the run
+            output_folder_run_path = output_folder_path+"/"+str(r+1)
+            os.makedirs(output_folder_run_path)
 
-        # print result
-        best = study.best_params
-        print(f"Solution is {best} for a value of {study.best_value}")
+            # write a txt log file with algorithm configuration and other execution details
+            log_file = open(f"{output_folder_run_path}/log.txt", 'w')
+            log_file.write(f"algorithm: optuna\n")
+            log_file.write(f"current date and time: {datetime.now().strftime('%Y%m%d_%H%M%S')}\n")
+            log_file.write(f"n_trials: {args['n_trials']}\n")
+            log_file.write(f"timeout: {args['timeout']}\n")
+            log_file.write(f"no_runs: {args['no_runs']}\n")
+            log_file.write(f"m: {args['m']}\n")
+            log_file.write(f"e: {args['e']}\n")
+            log_file.write(f"r: {args['r']}\n")
+            log_file.write(f"dataset: {args['dataset']}\n")
+            log_file.write(f"\n===============\n")
+
+            # history of evaluations
+            history_x:List[List[int]] = []
+            history_y:List[int] = []
+
+            # run the optimizer
+            study = optuna.create_study(direction="minimize", sampler=optuna.samplers.TPESampler(seed=r))
+            if args["n_trials"]:
+                study.optimize(
+                    lambda trial:   objective(  trial=trial,
+                                                jobs=input_jobs,
+                                                totalAvailableM=args["m"],
+                                                totalAvailableE=args["e"],
+                                                totalAvailableR=args["r"],
+                                                datasetFilePath=args["dataset"],
+                                                history_x=history_x,
+                                                history_y=history_y),
+                                    n_trials=args["n_trials"])
+            
+            if args["timeout"]:
+                study.optimize(
+                    lambda trial:   objective(  trial=trial,
+                                                jobs=input_jobs,
+                                                totalAvailableM=args["m"],
+                                                totalAvailableE=args["e"],
+                                                totalAvailableR=args["r"],
+                                                datasetFilePath=args["dataset"],
+                                                history_x=history_x,
+                                                history_y=history_y),
+                                    timeout=args["timeout"])
+
+            # print result
+            best = study.best_params
+            print(f"Solution is {best} for a value of {study.best_value}")
+            log_file.write(f"Solution is {best} for a value of {study.best_value}\n")
+
+            # store fitness trend history in csv output file
+            csv_file_path = f"{output_folder_run_path}/history_optuna.csv"
+            csv_file = open(csv_file_path, mode='w', newline='')
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["x", "y"])
+            for x, y in zip(history_x, history_y):
+                csv_writer.writerow([str(x), y])
+            csv_file.close()
+            print(f"Data successfully written to history_optuna.csv")
+            log_file.write(f"Data successfully written to history_optuna.csv\n")
+            log_file.close()
+        
+        # close model
+        adige_model.close()
 
         # run simulation with optimal result to use UI to explore results in AnyLogic
         #best_x = list(best.values())
         #simulation(x=best_x,
-                   #totalAvailableM=args["m"],
-                   #totalAvailableE=args["e"],
-                   #totalAvailableR=args["r"],
-                   #datasetFilePath=args["dataset"],
-                   #reset=False)
+                #totalAvailableM=args["m"],
+                #totalAvailableE=args["e"],
+                #totalAvailableR=args["r"],
+                #datasetFilePath=args["dataset"],
+                #reset=False)
 
         # solo per test
         """
@@ -148,16 +211,3 @@ if __name__ == '__main__':
                                 reset=False)
         print(f"makespan = {modelOutput}")
         """
-        
-        # close model
-        adige_model.close()
-
-        # store fitness trend history in csv output file
-        csv_file_path = "history_optuna.csv"
-        csv_file = open(csv_file_path, mode='w', newline='')
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["x", "y"])
-        for x, y in zip(history_x, history_y):
-            csv_writer.writerow([str(x), y])
-        csv_file.close()
-        print(f"Data successfully written to history_optuna.csv")
