ab3ee92a89b38e01f6720977871215fea63d4361
Stefano Genetti <stefanogenetti2000@gmail.com>
2024-09-04 11:57:56 +0200

    execution time and best result so far

--- optimization_optuna.py
+++ optimization_optuna.py
@@ -8,6 +8,7 @@ import csv
 import argparse
 import os
 import pandas as pd
+import time
 
 def read_arguments():
     parser = argparse.ArgumentParser(description="Adige hybrid flow shop scheduling optimizer.")
@@ -37,7 +38,7 @@ if __name__ == '__main__':
         csv_file_path = args["input_csv"]
         df = pd.read_csv(csv_file_path)
         plt.figure(figsize=(10, 6))
-        plt.plot(range(len(df["y"])), df["y"], marker='o', linestyle='-', linewidth=1, markersize=4, label='makespan')
+        plt.plot(range(len(df["y"])), df["y"].cummin(), marker='o', linestyle='-', linewidth=1, markersize=4, label='makespan')
         plt.xlabel('Trial')
         plt.ylabel('Makespan')
         plt.title('Fitness Trend')
@@ -124,6 +125,13 @@ if __name__ == '__main__':
             history_y.append(int(y))
             return y
 
+        # execution time csv file
+        csv_execution_time_file_path = f"{output_folder_path}/exec_optuna.csv"
+        csv_execution_time_file = open(csv_execution_time_file_path, mode='w', newline='')
+        csv_execution_time_writer = csv.writer(csv_execution_time_file)
+        csv_execution_time_writer.writerow(["run", "time"])
+        csv_execution_time_file.close()
+
         for r in range(args["no_runs"]):
             # create directory for saving results of the run
             output_folder_run_path = output_folder_path+"/"+str(r+1)
@@ -149,6 +157,7 @@ if __name__ == '__main__':
             # run the optimizer
             study = optuna.create_study(direction="minimize", sampler=optuna.samplers.TPESampler(seed=r))
             if args["n_trials"]:
+                start_time = time.time()
                 study.optimize(
                     lambda trial:   objective(  trial=trial,
                                                 jobs=input_jobs,
@@ -159,7 +168,12 @@ if __name__ == '__main__':
                                                 history_x=history_x,
                                                 history_y=history_y),
                                     n_trials=args["n_trials"])
-            
+                execution_time = time.time()-start_time
+                # store execution time of the run
+                csv_execution_time_file = open(csv_execution_time_file_path, mode='a', newline='')
+                csv_execution_time_writer = csv.writer(csv_execution_time_file)
+                csv_execution_time_writer.writerow([r, execution_time])
+                csv_execution_time_file.close()
             if args["timeout"]:
                 study.optimize(
                     lambda trial:   objective(  trial=trial,
